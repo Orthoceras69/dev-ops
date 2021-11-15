b874d22e4e8859edc3976f3cfaed4cd01d7edf15
Orthoceras <florian.berthier@hotmail.com>
2021-11-15 12:36:54 +0100

    Test Push index

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+include('_inc/top.php');
+?>
+<meta name="robots" content="all" />
+<meta name="description" content="description" />
+<title>Test Git - DevOps</title>
+</head>
+
+<body>
+	<?php
+	$nav_en_cours = 'index';
+	include('_inc/header.php');
+	?>
+	
+	<?php
+	include('_inc/footer.php');
+	?>
+	<?php
+	include('_inc/bottom.php');
+	?>
